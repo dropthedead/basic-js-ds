3d355407fbcd4e00a6b3bf32484eecf712c35879
dropthedead <dropdeaddropdead@yandex.ru>
2023-12-18 11:25:42 +0300

    feat: done queue, stack, list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,10 +22,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  if (!l) {
+    throw new Error('Invalid input. Linked list must be provided.');
+  }
+
+  const dNode = new ListNode(0);
+  dNode.next = l;
+
+  let current = dNode;
+
+  while (current.next) {
+    if (current.next.value === k) {
+      current.next = current.next.next; // Skip the node with value k
+    } else {
+      current = current.next;
+    }
+  }
+
+  return dNode.next;
 }
+
 
 module.exports = {
   removeKFromList
